c7943b52a446b3896ef9a902207996bbd0618094
viniproplayer <cezar.vinicius@escola.pr.gov.br>
2026-03-20 15:28:47 -0300

    Verificador de idade - aprendendo

---
+++ App.js
@@ -0,0 +1,13 @@
+const anoNascimento = 2005;
+const anoAtual = 2026;
+const  idade = anoAtual - anoNascimento;
+
+const.log("sua idade é: " + idade);
+
+if (idade >= 18) {
+  console.log ("Você não é patinho parabéns!!.");
+} else if (idade >= 16) {
+  console.log ("Você é um patinho vaza!!.");
+} else {
+  console.log ("Você é muito patinho.");
+}
